0bd4d20375dab16dfa7d4652957f26f37fb53fdc
azizthiampro@gmail.com <macbookm12021@macbookaziz.home>
2023-07-28 17:10:18 +0000

    Zone saisie de texte update

--- lib/zonetexte.dart
+++ lib/zonetexte.dart
@@ -1,373 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Zonetexte());
-
 class Zonetexte extends StatelessWidget {
+  const Zonetexte({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      title: 'Retrieve Text Input',
       debugShowCheckedModeBanner: false,
-      home: KeyboardDemo(),
+      home: ZonetexteForm(),
     );
   }
 }
 
-class KeyboardDemo extends StatefulWidget {
+// Define a custom Form widget.
+class ZonetexteForm extends StatefulWidget {
+  const ZonetexteForm({super.key});
+
   @override
-  _KeyboardDemoState createState() => _KeyboardDemoState();
+  State<ZonetexteForm> createState() => _ZonetexteFormState();
 }
 
-class _KeyboardDemoState extends State<KeyboardDemo> {
-  TextEditingController _controller = TextEditingController();
-  bool _readOnly = true;
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _ZonetexteFormState extends State<ZonetexteForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          SizedBox(height: 100),
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(3),
-              ),
-            ),
-            style: TextStyle(fontSize: 24),
-            autofocus: true,
-            showCursor: true,
-            readOnly: _readOnly,
-          ),
-          Spacer(),
-          CustomKeyboard(
-            onTextInput: (myText) {
-              _insertText(myText);
-            },
-            onBackspace: () {
-              _backspace();
-            },
-          ),
-          SizedBox(height: 50),
-        ],
-      ),
-    );
-  }
-
-  void _insertText(String myText) {
-    final text = _controller.text;
-    final textSelection = _controller.selection;
-    final newText = text.replaceRange(
-      textSelection.start,
-      textSelection.end,
-      myText,
-    );
-    final myTextLength = myText.length;
-    _controller.text = newText;
-    _controller.selection = textSelection.copyWith(
-      baseOffset: textSelection.start + myTextLength,
-      extentOffset: textSelection.start + myTextLength,
-    );
-  }
-
-  void _backspace() {
-    final text = _controller.text;
-    final textSelection = _controller.selection;
-    final selectionLength = textSelection.end - textSelection.start;
-
-    // There is a selection.
-    if (selectionLength > 0) {
-      final newText = text.replaceRange(
-        textSelection.start,
-        textSelection.end,
-        '',
-      );
-      _controller.text = newText;
-      _controller.selection = textSelection.copyWith(
-        baseOffset: textSelection.start,
-        extentOffset: textSelection.start,
-      );
-      return;
-    }
-
-    // The cursor is at the beginning.
-    if (textSelection.start == 0) {
-      return;
-    }
-
-    // Delete the previous character
-    final previousCodeUnit = text.codeUnitAt(textSelection.start - 1);
-    final offset = _isUtf16Surrogate(previousCodeUnit) ? 2 : 1;
-    final newStart = textSelection.start - offset;
-    final newEnd = textSelection.start;
-    final newText = text.replaceRange(
-      newStart,
-      newEnd,
-      '',
-    );
-    _controller.text = newText;
-    _controller.selection = textSelection.copyWith(
-      baseOffset: newStart,
-      extentOffset: newStart,
-    );
-  }
-
-  bool _isUtf16Surrogate(int value) {
-    return value & 0xF800 == 0xD800;
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-}
-
-class CustomKeyboard extends StatelessWidget {
-  CustomKeyboard({
-    Key? key,
-    required this.onTextInput,
-    required this.onBackspace,
-  }) : super(key: key);
-
-  final ValueSetter<String> onTextInput;
-  final VoidCallback onBackspace;
-
-  void _textInputHandler(String text) => onTextInput?.call(text);
-
-  void _backspaceHandler() => onBackspace?.call();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 160,
-      color: Color.fromARGB(255, 235, 235, 235),
-      child: Column(
-        children: [
-          buildRowOne(),
-          buildRowTwo(),
-          buildRow2(),
-          buildRow22(),
-          buildRowThree()
-        ],
-      ),
-    );
-  }
-
-  Expanded buildRowOne() {
-    return Expanded(
-      child: Row(
-        children: [
-          TextKey(
-            text: 'v',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'a',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'f',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'g',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'h',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'w',
-            onTextInput: _textInputHandler,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Expanded buildRowTwo() {
-    return Expanded(
-      child: Row(
-        children: [
-          TextKey(
-            text: 'k',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'l',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'm',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'n',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'p',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'z',
-            onTextInput: _textInputHandler,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Expanded buildRow2() {
-    return Expanded(
-      child: Row(
-        children: [
-          TextKey(
-            text: 'ɓ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ɖ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ɛ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ƒ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ɣ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ŋ',
-            onTextInput: _textInputHandler,
-          )
-        ],
-      ),
-    );
-  }
-
-  Expanded buildRow22() {
-    return Expanded(
-      child: Row(
-        children: [
-          TextKey(
-            text: '\'',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ɔ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ʋ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ʃ	',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'ʒ',
-            onTextInput: _textInputHandler,
-          ),
-          TextKey(
-            text: 'x',
-            onTextInput: _textInputHandler,
-          )
-        ],
-      ),
-    );
-  }
-
-  Expanded buildRowThree() {
-    return Expanded(
-      child: Row(
-        children: [
-          TextKey(
-            text: ' ',
-            flex: 4,
-            onTextInput: _textInputHandler,
-          ),
-          BackspaceKey(
-            onBackspace: _backspaceHandler,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TextKey extends StatelessWidget {
-  const TextKey({
-    Key? key,
-    required this.text,
-    required this.onTextInput,
-    this.flex = 1,
-  }) : super(key: key);
-
-  final String text;
-  final ValueSetter<String> onTextInput;
-  final int flex;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: flex,
-      child: Padding(
-        padding: const EdgeInsets.all(1.0),
-        child: Material(
-          color: Color.fromARGB(255, 213, 215, 216),
-          child: InkWell(
-            onTap: () {
-              onTextInput?.call(text);
-            },
-            child: Container(
-              child: Center(child: Text(text)),
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: myController,
         ),
       ),
-    );
-  }
-}
-
-class BackspaceKey extends StatelessWidget {
-  const BackspaceKey({
-    Key? key,
-    required this.onBackspace,
-    this.flex = 1,
-  }) : super(key: key);
-
-  final VoidCallback onBackspace;
-  final int flex;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: flex,
-      child: Padding(
-        padding: const EdgeInsets.all(1.0),
-        child: Material(
-          color: Color.fromARGB(255, 213, 215, 216),
-          child: InkWell(
-            onTap: () {
-              onBackspace?.call();
+      floatingActionButton: FloatingActionButton(
+        // When the user presses the button, show an alert dialog containing
+        // the text that the user has entered into the text field.
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                // Retrieve the text the that user has entered by using the
+                // TextEditingController.
+                content: Text(
+                  myController.text,
+                  style: TextStyle(fontFamily: 'Asa'),
+                ),
+              );
             },
-            child: Container(
-              child: Center(
-                child: Icon(Icons.backspace),
-              ),
-            ),
-          ),
-        ),
+          );
+        },
+        tooltip: 'Show me the value!',
+
+        child: const Icon(Icons.text_fields),
       ),
     );
   }
